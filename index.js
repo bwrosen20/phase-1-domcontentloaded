dcac861446e49f443834b2baf8ed9907ef1f4602
bwrosen20 <bwrosen20@gmail.com>
2023-01-02 13:53:57 -0500

    commit tree 31b221c8f279708895e74eec764213e718c8da74

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", editor)
+
+
+function editor() {
+    
+   let element=document.getElementById("text");
+    element.innerText=('This is really cool!');
+}
